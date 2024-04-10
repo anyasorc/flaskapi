2d9e3d9af96612c11d3b105197a495af58e85b90
Chigozie Anyasor <31804838+anyasorc@users.noreply.github.com>
2024-04-10 17:55:25 +0100

    changes to app.py and readme
    I made changes to the app.py file and the readme file respectively.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # API Development with Flask
-A python application powered by flask framework to run as an API
+A python application powered by flask framework to run as an API.
+The purpose for this application is to show how to run convert your python applicaiton to an API that can be called via Postman or any application.
--- app.py
+++ app.py
@@ -1,30 +1,16 @@
+from flask import Flask, request, jsonify
 
-from flask import Flask, request
 app = Flask(__name__)
-@app.route('/api/myapi', methods=['POST'])
-def predict():
-       # Get the JSON data from the request
-       data = request.get_json(force=True)
-       Amount = float(data['Amount'])
-       AccountNumber = data['AccountNumber']
-       # Assuming the machine learning model was named model
-       # prediction = model.predict(np.array([[Amount]]))
-       # output = prediction
-       output = 1
-       if output == 1:
-            remark = "The transaction amount is suspicious"
-       else:
-           remark = "The transaction amount is not suspicious"
-       rsp = {
-            'Prediction': str(output),
-            'AccountNumber': AccountNumber,
-            'Amount': Amount,
-            'remark': remark
-       }
-       return rsp
-	
-if __name__ == '__main__':
-    # Run using Waitress for production
-    from waitress import serve
-    serve(app, host='0.0.0.0', port=8080)
 
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    return jsonify(message="Hello, World!")
+
+@app.route('/api/echo', methods=['POST'])
+def echo():
+    data = request.json  # Assuming JSON data is sent
+    amt = data['amt']
+    return jsonify(amt)
+
+if __name__ == '__main__':
+    app.run(debug=True)
